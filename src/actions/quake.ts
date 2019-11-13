203ff483d8565c7bcd2dd0e41a799a39a73d24ac
Hal Peters <geohalbert@gmail.com>
2019-11-13 15:55:23 -0600

    changing api configuration

--- src/actions/quake.ts
+++ src/actions/quake.ts
@@ -10,18 +10,33 @@ import {
   QUAKE_FAILURE
 } from '../types';
 
-const API_URL = 'https://jsonplaceholder.typicode.com/quakes';
+function today() {
+  var d = new Date(),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+// const API_URL = 'https://jsonplaceholder.typicode.com/users';
+const API_URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-11-12&endtime=2019-11-13&limit=10`;
 // Export this for unit testing
 /* istanbul ignore next */
 export const fetchQuake = (
+  // userId: string,
   quakeId: string,
   URL: string = API_URL
 ): ThunkAction => async (dispatch: ThunkDispatch) => {
+  //dispatch({ type: QUAKE_REQUESTING, userId });
   dispatch({ type: QUAKE_REQUESTING, quakeId });
-
   try {
-    const { data } = await axios.get(`${URL}/${quakeId}`);
+    // const { data } = await axios.get(`${URL}/${userId}`);
+    const { data } = await axios.get(`${URL}&eventId=${quakeId}`);
 
     /* istanbul ignore next */
     dispatch({ type: QUAKE_SUCCESS, quakeId, data });
